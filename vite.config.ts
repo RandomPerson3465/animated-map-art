ca791adb97d9327d67e57d683cb363125b3c4424
RandomPerson3465 <50644934+RandomPerson3465@users.noreply.github.com>
2026-07-21 13:06:30 -0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/animated-map-art/'
+    base: '/animated-map-art/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                combine: resolve(__dirname, 'combine.html')
+            }
+        }
+    }
 });
